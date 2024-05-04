2ae760be7baf1a04be529f62a3d3ab30b6634884
Davey Boyle <davey@elucidan.com>
2024-05-04 12:11:23 +0100

    changed my colour scheme, how lovely

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,3 +1,19 @@
 return {
-  'Mofiqul/vscode.nvim',
+--'Mofiqul/vscode.nvim',
+{
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("cyberdream").setup({
+            -- Recommended - see "Configuring" below for more config options
+            transparent = true,
+            italic_comments = true,
+            hide_fillchars = true,
+            borderless_telescope = true,
+            terminal_colors = true,
+        })
+        vim.cmd("colorscheme cyberdream") -- set the colorscheme
+    end,
+},
 }
