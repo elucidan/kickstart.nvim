4891941f162a9f45e1946dd31b552391b43e0619
Davey Boyle <davey@elucidan.com>
2024-05-14 20:35:24 +0100

    Added tmux pane swapping stuff

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -92,16 +92,14 @@ vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 vim.keymap.set('n', ';', ':', { desc = 'CMD enter command mode' })
 vim.keymap.set('i', 'jk', '<ESC>')
 vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<CR>')
-
+vim.keymap.set('n', '<left>', '<c-w>5<')
+vim.keymap.set('n', '<right>', '<c-w>5>')
+vim.keymap.set('n', '<up>', '<C-W>+')
+vim.keymap.set('n', '<down>', '<C-W>-')
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
