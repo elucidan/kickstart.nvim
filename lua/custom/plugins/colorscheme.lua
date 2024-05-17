a1423a331a00d9ab35510f7ff1b22cd7c24e87d9
David Boyle <davey@elucidan.com>
2024-05-17 11:24:26 +0100

    these config changes are coming together now

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,4 +1,18 @@
 return {
-  --'Mofiqul/vscode.nvim',
-    'mhinz/vim-janah',
+  'Mofiqul/vscode.nvim',
+  'mhinz/vim-janah',
+  {
+    'fynnfluegge/monet.nvim',
+    name = 'monet',
+    config = function()
+      require('monet').setup {
+        transparent_background = true,
+        semantic_tokens = true,
+        dark_mode = true,
+        highlight_overrides = {},
+        color_overrides = {},
+        styles = {},
+      }
+    end,
+  },
 }
